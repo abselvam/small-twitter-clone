dc9d509ccda9d4c3bf02c83a1370b8ec690532ef
Abhijith Selvam <abselvam44@gmail.com>
2025-10-04 15:47:04 +0530

    3- Like post muttion added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -100,7 +100,10 @@ export const likeUnlikePost = async (req, res) => {
                 { $pull: { likedPosts: postId } },
                 { new: true }
             )
-            res.status(200).json({error: "Post uliked successfully"})
+
+            const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString())
+
+            res.status(200).json(updatedLikes)
         }else{
             //like post
             post.likes.push(userId)
@@ -118,7 +121,8 @@ export const likeUnlikePost = async (req, res) => {
             })
             await notification.save()
 
-            res.status(200).json({message: "Post liked successfully"})
+            const updatedLikes = post.likes
+            res.status(200).json(updatedLikes)
         }
     } catch (error) {
         console.log("Error in likeUnlikePost controller:", error.message)
